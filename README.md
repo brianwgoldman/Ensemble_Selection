b024021c7b77c0f571337e625bdd756da30dbc6e
Brian W. Goldman <brianwgoldman@acm.org>
2016-08-10 11:17:44 -0600

    Made data_manager more flexible, updated README

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Ensemble_Selection
 
-Requires the "scikit learn" python module
+
+## Dependencies
+Requires the `scikit learn` python module, which depends on `numpy` and `scipy`.
+
+On the CS machines, you will need to edit your .bashrc to include the following
+```
+export PYTHONPATH="$PYTHONPATH:/usr/local/numpy-1.11.1/lib64/python2.7/site-packages"
+export PYTHONPATH="$PYTHONPATH:/usr/local/scipy-0.17.1/lib64/python2.7/site-packages"
+export PYTHONPATH="$PYTHONPATH:/usr/local/scikit-learn-0.17.1_fc24/lib64/python2.7/site-packages"
+```
+
+## Getting the data formatted
+This will combine all of the images of the same class into a single binary file that numpy knows how to read.
+
+```python data_manager.py create_class_files -input_folder validation/ -output_folder compressed -groundtruth_file validation/groundtruth.txt```
+
+This will create a "problem" with 41 random classes (instead of the full 1000).
+
+```python data_manager.py create_subproblem -input_folder compressed/ -num_classes 41 -output_file problem_with_41_classes.txt```
--- data_manager.py
+++ data_manager.py
@@ -1,18 +1,18 @@
 from __future__ import print_function
 import numpy as np
 from glob import glob
-from os import path
+from os import path, makedirs
 from collections import defaultdict
 
-def create_subproblem(folder, num_classes, output_filename):
-    class_files = glob(path.join(folder, "*.npy"))
+def create_subproblem(input_folder, num_classes, output_filename, **_):
+    class_files = glob(path.join(input_folder, "*.npy"))
     subset_files = np.random.choice(class_files, num_classes, replace=False)
     with open(output_filename, 'w') as f:
         for filename in subset_files:
             print(filename, file=f)
 
-def load_subproblem(filename):
-    with open(filename, 'r') as f:
+def load_subproblem(input_filename, **_):
+    with open(input_filename, 'r') as f:
         subset_files = f.read().strip().split()
     target = []
     for filename in subset_files:
@@ -20,6 +20,8 @@ def load_subproblem(filename):
             class_data = np.load(f)
         class_label = path.splitext(path.basename(filename))[0]
         target.extend([class_label] * class_data.shape[0])
+        # This try except block is used to create "data" initialized
+        # to the very first file's "class_data"
         try:
             data = np.vstack([data, class_data])
         except NameError:
@@ -28,7 +30,8 @@ def load_subproblem(filename):
     target = np.array(target)
     return data, target
 
-def create_class_files(groundtruth_file, input_folder, output_folder):
+def create_class_files(groundtruth_file, input_folder, output_folder, **_):
+    makedirs(output_folder) # TODO Consider providing helpful error messages
     with open(groundtruth_file, 'r') as f:
         lines = f.read().strip().split('\n')
 
@@ -51,10 +54,16 @@ def create_class_files(groundtruth_file, input_folder, output_folder):
             np.save(f, data)
 
 if __name__ == '__main__':
-    # TODO Probably should argparse this
-    # TODO Allow command line switching between functions
     import sys
-    folder = sys.argv[1]
-    num_classes = int(sys.argv[2])
-    output_filename = sys.argv[3]
-    create_subproblem(folder, num_classes, output_filename)
+    import argparse
+    parser = argparse.ArgumentParser(description="Munge data from Dhruva")
+    parser.add_argument("tool")
+    parser.add_argument('-input_folder', type=str)
+    parser.add_argument('-num_classes', type=int)
+    parser.add_argument('-output_filename', type=str)
+    parser.add_argument('-input_filename', type=str)
+    parser.add_argument('-groundtruth_file', type=str)
+    parser.add_argument('-output_folder', type=str)
+    command_line = vars(parser.parse_args())
+    # TODO Make better error handling and help messages
+    locals()[command_line['tool']](**command_line)
